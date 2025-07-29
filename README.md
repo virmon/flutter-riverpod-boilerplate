d5ad8a306da4b3a7dc834039effdf50340c060a8
virmon <virmontumulak@gmail.com>
2025-07-29 14:31:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter Riverpod Boilerplate
+
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_boilerplate/src/app.dart';
+
+void main() {
+  runApp(const App());
+}
