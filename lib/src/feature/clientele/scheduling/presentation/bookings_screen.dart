f24942a17981dcf6349d257d8261bd4b00bfa4cb
virmon <virmontumulak@gmail.com>
2025-08-11 13:37:16 +0800

    feat: added desktop view for bookings screen

--- lib/src/feature/clientele/scheduling/presentation/bookings_screen.dart
+++ lib/src/feature/clientele/scheduling/presentation/bookings_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_boilerplate/src/common/async_value_widget.dart';
-import 'package:flutter_riverpod_boilerplate/src/common/booking_card_widget.dart';
-import 'package:flutter_riverpod_boilerplate/src/common/carousel_widget.dart';
+import 'package:flutter_riverpod_boilerplate/src/constants/app_colors.dart';
 import 'package:flutter_riverpod_boilerplate/src/feature/clientele/scheduling/data/fake_app_user_repository.dart';
 import 'package:flutter_riverpod_boilerplate/src/feature/clientele/scheduling/data/fake_memberships_repository.dart';
+import 'package:flutter_riverpod_boilerplate/src/feature/clientele/scheduling/presentation/m_bookings_screen.dart';
 import 'package:flutter_riverpod_boilerplate/src/routing/clientele/clientele_router.dart';
 import 'package:go_router/go_router.dart';
+
+class ExpansionItem {
+  ExpansionItem({this.isExpanded = false});
+
+  bool isExpanded;
+}
 
 class BookingsScreen extends ConsumerStatefulWidget {
   const BookingsScreen({super.key});
@@ -16,156 +22,250 @@ class BookingsScreen extends ConsumerStatefulWidget {
 }
 
 class _BookingsScreenState extends ConsumerState<BookingsScreen> {
+  bool bookNowIsExpanded = true;
+  bool upcomingBookingsIsExpanded = true;
+  bool pastBookingsIsExpanded = true;
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isMobileView = screenWidth < 600;
+
     final membershipsAsyncValue = ref.watch(membershipsListFutureProvider);
     final upcomingBookingsAsyncValue = ref.watch(
       upcomingBookingsListFutureProvider,
     );
     final pastBookingsAsyncValue = ref.watch(pastBookingsListFutureProvider);
 
-    return AsyncValueWidget(
-      value: membershipsAsyncValue,
-      data: (memberships) => ListView(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                child: Text(
-                  'Book Now',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 10,
+    if (isMobileView) {
+      return MBookingsScreen();
+    } else {
+      return Scaffold(
+        body: AsyncValueWidget(
+          value: membershipsAsyncValue,
+          data: (memberships) => SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.all(50.0),
+              width: 1080,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'My Bookings',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
                   ),
-                  child: Row(
-                    spacing: 25.0,
-                    children: memberships
-                        .map(
-                          (membership) => MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: GestureDetector(
-                              onTap: () {
-                                if (membership.businessId != null) {
-                                  context.goNamed(
-                                    ClienteleRoute.tenantCalendar.name,
-                                    pathParameters: {
-                                      'businessId': membership.businessId
-                                          .toString(),
-                                    },
-                                  );
-                                }
+                  SizedBox(height: 40),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: BoxBorder.all(color: AppColors.lightGrey),
+                      borderRadius: BorderRadius.circular(3.0),
+                    ),
+                    child: ExpansionPanelList(
+                      expansionCallback: (int index, bool isExpanded) {
+                        setState(() {
+                          bookNowIsExpanded = isExpanded;
+                        });
+                      },
+                      children: [
+                        ExpansionPanel(
+                          headerBuilder:
+                              (BuildContext context, bool isExpanded) {
+                                return ListTile(
+                                  title: Text(
+                                    'Book Now',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                );
                               },
-                              child: Column(
-                                children: [
-                                  CircleAvatar(
-                                    radius: 30,
-                                    backgroundImage: AssetImage(
+                          body: Column(
+                            children: memberships.map((membership) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  border: BoxBorder.fromLTRB(
+                                    top: BorderSide(color: AppColors.lightGrey),
+                                  ),
+                                ),
+                                child: ListTile(
+                                  minTileHeight: 60,
+                                  leading: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Image.asset(
                                       'assets/avatar_placeholder2.jpg',
                                     ),
                                   ),
-                                  Text(
+                                  title: Text(
                                     membership.name.toString(),
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w200,
+                                    style: TextStyle(color: AppColors.violet99),
+                                  ),
+                                  onTap: () {
+                                    if (membership.businessId != null) {
+                                      context.goNamed(
+                                        ClienteleRoute.tenantCalendar.name,
+                                        pathParameters: {
+                                          'businessId': membership.businessId
+                                              .toString(),
+                                        },
+                                      );
+                                    }
+                                  },
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                          isExpanded: bookNowIsExpanded,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  AsyncValueWidget(
+                    value: upcomingBookingsAsyncValue,
+                    data: (bookings) => Container(
+                      decoration: BoxDecoration(
+                        border: BoxBorder.all(color: AppColors.lightGrey),
+                        borderRadius: BorderRadius.circular(3.0),
+                      ),
+                      child: ExpansionPanelList(
+                        expansionCallback: (int index, bool isExpanded) {
+                          setState(() {
+                            upcomingBookingsIsExpanded = isExpanded;
+                          });
+                        },
+                        children: <ExpansionPanel>[
+                          ExpansionPanel(
+                            headerBuilder:
+                                (BuildContext context, bool isExpanded) {
+                                  return ListTile(
+                                    title: Text(
+                                      'Upcoming',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  );
+                                },
+                            body: Column(
+                              children: bookings.map((booking) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    border: BoxBorder.fromLTRB(
+                                      top: BorderSide(
+                                        color: AppColors.lightGrey,
+                                      ),
                                     ),
                                   ),
-                                ],
-                              ),
+                                  child: ListTile(
+                                    title: Text(
+                                      booking.title.toString(),
+                                      style: TextStyle(
+                                        color: AppColors.violet99,
+                                      ),
+                                    ),
+                                    subtitle: Text('Hosted by Business'),
+                                    trailing: Text(
+                                      booking.startTime.toString(),
+                                      style: TextStyle(
+                                        color: AppColors.lightGrey,
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      context.goNamed(
+                                        ClienteleRoute.bookingDetail.name,
+                                        pathParameters: {
+                                          'blockId': booking.blockId.toString(),
+                                        },
+                                      );
+                                    },
+                                  ),
+                                );
+                              }).toList(),
                             ),
+                            isExpanded: upcomingBookingsIsExpanded,
                           ),
-                        )
-                        .toList(),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                child: Text(
-                  'Upcoming',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-              ),
-              AsyncValueWidget(
-                value: upcomingBookingsAsyncValue,
-                data: (booking) => Carousel(
-                  children: booking
-                      .map(
-                        (booking) => GestureDetector(
-                          onTap: () {
-                            context.goNamed(
-                              ClienteleRoute.bookingDetail.name,
-                              pathParameters: {
-                                'blockId': booking.blockId.toString(),
-                              },
-                            );
-                          },
-                          child: BookingCardWidget(
-                            title: booking.title.toString(),
-                            startTime: booking.startTime.toString(),
-                            location: booking.block?.location.toString() ?? '',
-                            status: booking.status.toString(),
+                  SizedBox(height: 30),
+                  AsyncValueWidget(
+                    value: pastBookingsAsyncValue,
+                    data: (bookings) => Container(
+                      decoration: BoxDecoration(
+                        border: BoxBorder.all(color: AppColors.lightGrey),
+                        borderRadius: BorderRadius.circular(3.0),
+                      ),
+                      child: ExpansionPanelList(
+                        expansionCallback: (int index, bool isExpanded) {
+                          setState(() {
+                            pastBookingsIsExpanded = isExpanded;
+                          });
+                        },
+                        children: [
+                          ExpansionPanel(
+                            headerBuilder:
+                                (BuildContext context, bool isExpanded) {
+                                  return ListTile(
+                                    title: Text(
+                                      'Past',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  );
+                                },
+                            body: Column(
+                              children: bookings.map((booking) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    border: BoxBorder.fromLTRB(
+                                      top: BorderSide(
+                                        color: AppColors.lightGrey,
+                                      ),
+                                    ),
+                                  ),
+                                  child: ListTile(
+                                    title: Text(
+                                      booking.title.toString(),
+                                      style: TextStyle(
+                                        color: AppColors.violet99,
+                                      ),
+                                    ),
+                                    subtitle: Text('Hosted by Business'),
+                                    trailing: Text(
+                                      booking.startTime.toString(),
+                                      style: TextStyle(
+                                        color: AppColors.lightGrey,
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      context.goNamed(
+                                        ClienteleRoute.bookingDetail.name,
+                                        pathParameters: {
+                                          'blockId': booking.blockId.toString(),
+                                        },
+                                      );
+                                    },
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                            isExpanded: pastBookingsIsExpanded,
                           ),
-                        ),
-                      )
-                      .toList(),
-                ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                child: Text(
-                  'Past Bookings',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-              ),
-              AsyncValueWidget(
-                value: pastBookingsAsyncValue,
-                data: (pastBookings) => Column(
-                  children: pastBookings
-                      .map(
-                        (booking) => GestureDetector(
-                          onTap: () {
-                            context.goNamed(
-                              ClienteleRoute.bookingDetail.name,
-                              pathParameters: {
-                                'blockId': booking.blockId.toString(),
-                              },
-                            );
-                          },
-                          child: BookingCardWidget(
-                            title: booking.title.toString(),
-                            startTime: booking.startTime.toString(),
-                            location:
-                                booking.block?.location.toString() ??
-                                'no location',
-                            status: booking.status.toString(),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-            ],
+            ),
           ),
-        ],
-      ),
-    );
+        ),
+      );
+    }
   }
 }
